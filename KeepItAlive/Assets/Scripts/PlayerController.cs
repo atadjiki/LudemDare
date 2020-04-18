da466f761743540f9e686f51336267d23a924fce
Arash Tadjiki <atadjiki@gmail.com>
2020-04-18 15:18:02 -0600

    interactions and shit

--- KeepItAlive/Assets/Scripts/PlayerController.cs
+++ KeepItAlive/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody), typeof(Camera))]
 public class PlayerController : MonoBehaviour
 {
+    private static PlayerController _instance;
+
     [SerializeField] private float Speed_Pitch = 50;
     [SerializeField] private float Speed_Roll = 75;
     [SerializeField] private float Speed_Yaw = 50;
@@ -13,18 +15,79 @@ public class PlayerController : MonoBehaviour
     private float Lerp_Rotate = 50;
     private float Lerp_Move = 50;
 
+    public GameObject grabbableOffset;
+    [SerializeField] private float interactDistance = 1.0f;
+    [SerializeField] private float centerLerp = 0.01f;
+
+    private bool isHolding = false;
+    private Grabbable currentlyGrabbed = null;
+
     private Rigidbody Player_Rigidbody;
     private PlayerInputActions Actions;
     private Camera PlayerCamera;
 
+    public static PlayerController Instance { get { return _instance; } }
+
     private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        Build();
+    }
+
+    internal void Build()
     {
         Player_Rigidbody = GetComponent<Rigidbody>();
         Actions = new PlayerInputActions();
         PlayerCamera = GetComponent<Camera>();
 
+        Actions.Player.Interact.performed += ctx => Interact();
+
         Actions.Enable();
     }
+
+    private void Update()
+    {
+        if (isHolding)
+        {
+
+            if (currentlyGrabbed.gameObject.GetComponent<Rigidbody>() != null)
+            {
+                currentlyGrabbed.transform.localPosition = Vector3.Lerp(currentlyGrabbed.transform.localPosition, Vector3.zero, centerLerp);
+                currentlyGrabbed.transform.localRotation = Quaternion.Lerp(currentlyGrabbed.transform.localRotation, Quaternion.identity, centerLerp);
+                currentlyGrabbed.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                currentlyGrabbed.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+        }
+        else
+        {
+            Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.GetComponent<Interactable>() != null && Vector3.Distance(this.transform.position, hit.transform.position) <= interactDistance)
+                {
+                    UIManager.Instance.SetObjectInRange();
+                }
+                else
+                {
+                    UIManager.Instance.SetDefault();
+                }
+            }
+            else
+            {
+                UIManager.Instance.SetDefault();
+            }
+        }
+    }
+
 
     private void FixedUpdate()
     {
@@ -32,6 +95,70 @@ public class PlayerController : MonoBehaviour
         HandleYaw();
         HandleLateral();
         HandleVertical();
+
+        if(Actions.Player.Interact.triggered)
+        {
+            Interact();
+        }
+    }
+
+    internal void Interact()
+    {
+        //perform raycast to detect object
+        Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) && isHolding == false)
+        {
+            if (hit.transform.gameObject.GetComponent<Interactable>() != null && Vector3.Distance(this.transform.position, hit.transform.position) <= interactDistance)
+            {
+                if (hit.transform.gameObject.GetComponent<Grabbable>() != null)
+                {
+                    if (isHolding == false)
+                    {
+                        Grab(hit.transform.gameObject.GetComponent<Grabbable>());
+                    }
+
+                }
+                //else if (hit.transform.gameObject.GetComponent<__>())
+                //{
+                //    hit.transform.gameObject.GetComponent<__r>().Interact();
+                //}
+            }
+        }
+        else if (isHolding)
+        {
+            Release();
+        }
+    }
+
+    internal void Grab(Grabbable target)
+    {
+        isHolding = true;
+        currentlyGrabbed = target;
+
+        if (currentlyGrabbed.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            currentlyGrabbed.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+
+        currentlyGrabbed.transform.SetParent(grabbableOffset.transform);
+        UIManager.Instance.SetObjectGrabbed();
+    }
+
+    internal void Release()
+    {
+        Debug.Log("Releasing " + currentlyGrabbed.name);
+
+        if (currentlyGrabbed.gameObject.GetComponent<Rigidbody>() != null)
+        {
+            currentlyGrabbed.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+
+        currentlyGrabbed.transform.SetParent(null);
+
+        currentlyGrabbed = null;
+        isHolding = false;
+        UIManager.Instance.SetObjectReleased();
     }
 
     internal void HandlePitchRoll()
---
+++ KeepItAlive/Assets/Scripts/UIManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public RawImage crosshair;
+    public GameObject pre_interact_text;
+    public GameObject during_interact_text;
+
+    private static UIManager _instance;
+
+    public static UIManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private void ToggleCrosshair(bool flag)
+    {
+        crosshair.enabled = flag;
+    }
+
+    public void SetDefault()
+    {
+        crosshair.color = Color.white;
+        pre_interact_text.SetActive(false);
+        during_interact_text.SetActive(false);
+    }
+
+    public void SetObjectInRange()
+    {
+        crosshair.color = Color.green;
+        pre_interact_text.SetActive(true);
+        during_interact_text.SetActive(false);
+    }
+
+    public void SetObjectGrabbed()
+    {
+        ToggleCrosshair(false);
+        pre_interact_text.SetActive(false);
+        during_interact_text.SetActive(true);
+    }
+
+    public void SetObjectReleased()
+    {
+        ToggleCrosshair(true);
+    }
+}
